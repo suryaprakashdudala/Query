2e643aeda04a59fdda2af0a66a9d5be26adfdade
Satya  Suryaprakash Dudala <satya.suryaprakash.dudala@ey.com>
2026-02-27 23:42:56 +0530

    Added test results

---
+++ WorkingQueryResults.js
@@ -0,0 +1,65 @@
+[
+  {
+    _id: ObjectId('69a1de53da741ec1c4c5bc52'),
+    Role: 'UltimateResponsibilitySqmTitle',
+    Title: '0FFXSrFr',
+    AssignmentName: 'Rachi Garg: Rachi.Garg@gds.ey.com; Rachi Nelaturi: Rachi.Nelaturi@parthenon.ey.com',
+    Assignment: 'Rachi.Garg@gds.ey.com; Rachi.Nelaturi@parthenon.ey.com',
+    FY: 'FY26',
+    MemberFirmId: 10307
+  },
+  {
+    _id: ObjectId('69a1de53da741ec1c4c5bc53'),
+    Role: 'UltimateResponsibilitySqmTitle',
+    Title: '0OpnMPuD',
+    AssignmentName: 'Sub Group CMH: subgroup@ie.ey.com; Sub Moitra: Sub.Moitra@ey.com',
+    Assignment: 'subgroup@ie.ey.com; Sub.Moitra@ey.com',
+    FY: 'FY26',
+    MemberFirmId: 10307
+  },
+  {
+    _id: ObjectId('69a1de53da741ec1c4c5bc54'),
+    Role: 'OperationalResponsibilitySqmTitle',
+    Title: '07988974A&*',
+    AssignmentName: 'Satya K Ahir: satya.ahir@in.ey.com; Satya S Kumar Akella: Satya.Akella@gds.ey.com',
+    Assignment: 'satya.ahir@in.ey.com; Satya.Akella@gds.ey.com',
+    FY: 'FY26',
+    MemberFirmId: 10307
+  },
+  {
+    _id: ObjectId('69a1de53da741ec1c4c5bc55'),
+    Role: 'OperationalResponsibilitySqmTitle',
+    Title: '08y0jkuS',
+    AssignmentName: 'Surya A: Surya.A@gds.ey.com; Surya .: Surya@gds.ey.com',
+    Assignment: 'Surya.A@gds.ey.com; Surya@gds.ey.com',
+    FY: 'FY26',
+    MemberFirmId: 10307
+  },
+  {
+    _id: ObjectId('69a1de53da741ec1c4c5bc56'),
+    Role: 'OperationalResponsibilitySqmTitle',
+    Title: '09385520A&*',
+    AssignmentName: 'Prakasa Anantarapu: Prakasa.Anantarapu@sg.ey.com; Praksha Jaga: Praksha.Jaga@za.ey.com',
+    Assignment: 'Prakasa.Anantarapu@sg.ey.com; Praksha.Jaga@za.ey.com',
+    FY: 'FY26',
+    MemberFirmId: 10307
+  },
+  {
+    _id: ObjectId('69a1de53da741ec1c4c5bc57'),
+    Role: 'OperationalResponsibilityComplianceWithRequirementTitle',
+    Title: '0ql8mxlR',
+    Assignment: 'Deepak.11@in.ey.com; Deepak.Agarwal@ie.ey.com',
+    AssignmentName: 'Deepak .: Deepak.11@in.ey.com; Deepak Agarwal: Deepak.Agarwal@ie.ey.com',
+    FY: 'FY26',
+    MemberFirmId: 10307
+  },
+  {
+    _id: ObjectId('69a1de53da741ec1c4c5bc58'),
+    Role: 'OperationalResponsibilityMonitoringAndRemediationTitle',
+    Title: '0RGMWHQq',
+    AssignmentName: 'Praveen .: Praveen1@gds.ey.com; Praveen A Hosamani: Praveen.Hosmani@gds.ey.com',
+    Assignment: 'Praveen1@gds.ey.com; Praveen.Hosmani@gds.ey.com',
+    FY: 'FY26',
+    MemberFirmId: 10307
+  }
+]
