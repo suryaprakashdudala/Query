24df432e29cc8599b2f151548080ef9b36816db8
Satya  Suryaprakash Dudala <satya.suryaprakash.dudala@ey.com>
2026-03-13 19:20:54 +0530

    Updated query

---
+++ results of titleAndAssignments collection.js
@@ -0,0 +1,27 @@
+[
+  {
+    _id: ObjectId('69b41442694f50492e845716'),
+    fiscalYear: 2026,
+    titleId: '69044a794d636931b9cecba6',
+    titleName: '07988974A&*',
+    firmId: [ 'ARLO9', 'NTW', 'SRA', 'nloc1' ],
+    firmAssignments: [
+      {
+        firmId: 'ARLO9',
+        assignmentString: 'Satya Suryaprakash Dudala(Satya.Suryaprakash.Dudala@ey.com); Subha Korla(Subha.Korla@ey.com)'
+      },
+      {
+        firmId: 'NTW',
+        assignmentString: 'Satya Suryaprakash Dudala(Satya.Suryaprakash.Dudala@ey.com)'
+      },
+      {
+        firmId: 'SRA',
+        assignmentString: 'Satya Suryaprakash Dudala(Satya.Suryaprakash.Dudala@ey.com)'
+      },
+      {
+        firmId: 'nloc1',
+        assignmentString: 'Zeeshan Mohammad(Zeeshan.Mohammad@ey.com)'
+      }
+    ]
+  }
+]
--- sqmleadershiproles.js
+++ sqmleadershiproles.js
@@ -4,7 +4,6 @@ try{
     var startTime = new Date();
     print("SYSTEM:Power-BI-Info:: Starting SQM Leadership Roles script at ", startTime.toISOString());
     db.sqmleadership.drop();
-    db.title_prejoined.drop();
 
     var fiscalYearFilter = 2026;
     db.title.aggregate([
@@ -55,12 +54,11 @@ try{
                 }
             }
         },
-        { $out: "title_prejoined" }
+        { $out: "titleAndAssignments" }
     ]);
 
-    db.title_prejoined.createIndex({ titleId: 1, fiscalYear: 1 });
+    db.titleAndAssignments.createIndex({ titleId: 1, fiscalYear: 1 });
 
-    // 2. Main Aggregation
     db.firm.aggregate([
         {
             $match: {
@@ -70,32 +68,19 @@ try{
         },
         {
             $addFields: {
-                // Collect all possible leadership IDs to do a single lookup
                 lookupIds: {
                     $setUnion: [
                         { $ifNull: ["$ultimateResponsibility", []] },
                         { $ifNull: ["$operationalResponsibilitySqm", []] },
-                        { $cond: [{ $ne: ["$orIndependenceRequirement", null] }, ["$orIndependenceRequirement"], []] },
-                        { $cond: [{ $ne: ["$orMonitoringRemediation", null] }, ["$orMonitoringRemediation"], []] }
+                        { $cond: [{ $ne: ["$orIndependenceRequirement", null] }, [{$toString: "$orIndependenceRequirement"}], []] },
+                        { $cond: [{ $ne: ["$orMonitoringRemediation", null] }, [{$toString: "$orMonitoringRemediation"}], []] }
                     ]
                 }
             }
         },
         {
-            $addFields: {
-                // Convert IDs to strings for lookup matching
-                lookupIds: {
-                    $map: {
-                        input: "$lookupIds",
-                        as: "id",
-                        in: { $toString: "$$id" }
-                    }
-                }
-            }
-        },
-        {
             $lookup: {
-                from: 'title_prejoined',
+                from: 'titleAndAssignments',
                 let: { ids: "$lookupIds", fy: "$fiscalYear" },
                 pipeline: [
                     {
@@ -114,7 +99,6 @@ try{
         },
         {
             $addFields: {
-                // Filter the looked up leadership data into specific roles
                 ultimateResponsibility: {
                     $filter: {
                         input: "$leadership",
@@ -149,132 +133,150 @@ try{
             $addFields: {
                 combined: {
                     $concatArrays: [
-                        // Role 1 & 2: Return blank row if empty
                         {
                             $cond: [
                                 { $gt: [{ $size: "$ultimateResponsibility" }, 0] },
-                                { $map: {
-                                    input: "$ultimateResponsibility",
-                                    as: "r",
-                                    in: {
-                                        role: "ultimateResponsibility",
-                                        title: "$$r.titleName",
-                                        assignment: {
-                                            $reduce: {
-                                                input: {
-                                                    $filter: {
-                                                        input: "$$r.firmAssignments",
-                                                        as: "fa",
-                                                        cond: { $eq: ["$$fa.firmId", "$firmGroupId"] }
-                                                    }
-                                                },
-                                                initialValue: "",
-                                                in: { $concat: ["$$value", "$$this.assignmentString"] }
-                                            }
-                                        },
-                                        fiscalYear: "$fiscalYear",
-                                        memberFirmId: "$memberFirmId",
-                                        country: "$country",
-                                        firmGroupId: "$firmGroupId",
-                                        name: "$name",
-                                        type: "$type",
-                                        leadershipId: { $toObjectId: "$$r.titleId" }
-                                    }
-                                } },
+                                { 
+                                    $map: {
+                                        input: "$ultimateResponsibility",
+                                        as: "r",
+                                        in: {
+                                            role: "ultimateResponsibility",
+                                            title: "$$r.titleName",
+                                            assignment: {
+                                                $reduce: {
+                                                    input: {
+                                                        $filter: {
+                                                            input: "$$r.firmAssignments",
+                                                            as: "fa",
+                                                            cond: { $eq: ["$$fa.firmId", "$firmGroupId"] }
+                                                        }
+                                                    },
+                                                    initialValue: "",
+                                                    in: { $concat: ["$$value", "$$this.assignmentString"] }
+                                                }
+                                            },
+                                            fiscalYear: "$fiscalYear",
+                                            memberFirmId: "$memberFirmId",
+                                            country: "$country",
+                                            firmGroupId: "$firmGroupId",
+                                            name: "$name",
+                                            type: "$type",
+                                            leadershipId: { $toObjectId: "$$r.titleId" }
+                                        }
+                                    } 
+                                },
                                 [{ role: "ultimateResponsibility", title: "", assignment: "", fiscalYear: "$fiscalYear", memberFirmId: "$memberFirmId", country: "$country", firmGroupId: "$firmGroupId", name: "$name", type: "$type" }]
                             ]
                         },
                         {
                             $cond: [
                                 { $gt: [{ $size: "$operationalResponsibilitySqm" }, 0] },
-                                { $map: {
-                                    input: "$operationalResponsibilitySqm",
-                                    as: "r",
-                                    in: {
-                                        role: "operationalResponsibilitySqm",
-                                        title: "$$r.titleName",
-                                        assignment: {
-                                            $reduce: {
-                                                input: {
-                                                    $filter: {
-                                                        input: "$$r.firmAssignments",
-                                                        as: "fa",
-                                                        cond: { $eq: ["$$fa.firmId", "$firmGroupId"] }
-                                                    }
-                                                },
-                                                initialValue: "",
-                                                in: { $concat: ["$$value", "$$this.assignmentString"] }
-                                            }
-                                        },
-                                        fiscalYear: "$fiscalYear",
-                                        memberFirmId: "$memberFirmId",
-                                        country: "$country",
-                                        firmGroupId: "$firmGroupId",
-                                        name: "$name",
-                                        type: "$type",
-                                        leadershipId: { $toObjectId: "$$r.titleId" }
-                                    }
-                                } },
+                                { 
+                                    $map: {
+                                        input: "$operationalResponsibilitySqm",
+                                        as: "r",
+                                        in: {
+                                            role: "operationalResponsibilitySqm",
+                                            title: "$$r.titleName",
+                                            assignment: {
+                                                $reduce: {
+                                                    input: {
+                                                        $filter: {
+                                                            input: "$$r.firmAssignments",
+                                                            as: "fa",
+                                                            cond: { $eq: ["$$fa.firmId", "$firmGroupId"] }
+                                                        }
+                                                    },
+                                                    initialValue: "",
+                                                    in: { $concat: ["$$value", "$$this.assignmentString"] }
+                                                }
+                                            },
+                                            fiscalYear: "$fiscalYear",
+                                            memberFirmId: "$memberFirmId",
+                                            country: "$country",
+                                            firmGroupId: "$firmGroupId",
+                                            name: "$name",
+                                            type: "$type",
+                                            leadershipId: { $toObjectId: "$$r.titleId" }
+                                        }
+                                    } 
+                                },
                                 [{ role: "operationalResponsibilitySqm", title: "", assignment: "", fiscalYear: "$fiscalYear", memberFirmId: "$memberFirmId", country: "$country", firmGroupId: "$firmGroupId", name: "$name", type: "$type" }]
                             ]
                         },
-                        // Role 3 & 4: Return nothing if null
-                        { $map: {
-                            input: "$orIndependenceRequirement",
-                            as: "r",
-                            in: {
-                                role: "orIndependenceRequirement",
-                                title: "$$r.titleName",
-                                assignment: {
-                                    $reduce: {
-                                        input: {
-                                            $filter: {
-                                                input: "$$r.firmAssignments",
-                                                as: "fa",
-                                                cond: { $eq: ["$$fa.firmId", "$firmGroupId"] }
-                                            }
-                                        },
-                                        initialValue: "",
-                                        in: { $concat: ["$$value", "$$this.assignmentString"] }
-                                    }
+                        { 
+                            $cond: [
+                                { $gt: [{ $size: "$orIndependenceRequirement" }, 0] },
+                                {
+                                    $map: {
+                                        input: "$orIndependenceRequirement",
+                                        as: "r",
+                                        in: {
+                                            role: "orIndependenceRequirement",
+                                            title: "$$r.titleName",
+                                            assignment: {
+                                                $reduce: {
+                                                    input: {
+                                                        $filter: {
+                                                            input: "$$r.firmAssignments",
+                                                            as: "fa",
+                                                            cond: { $eq: ["$$fa.firmId", "$firmGroupId"] }
+                                                        }
+                                                    },
+                                                    initialValue: "",
+                                                    in: { $concat: ["$$value", "$$this.assignmentString"] }
+                                                }
+                                            },
+                                            fiscalYear: "$fiscalYear",
+                                            memberFirmId: "$memberFirmId",
+                                            country: "$country",
+                                            firmGroupId: "$firmGroupId",
+                                            name: "$name",
+                                            type: "$type",
+                                            leadershipId: { $toObjectId: "$$r.titleId" }
+                                        }
+                                    } 
                                 },
-                                fiscalYear: "$fiscalYear",
-                                memberFirmId: "$memberFirmId",
-                                country: "$country",
-                                firmGroupId: "$firmGroupId",
-                                name: "$name",
-                                type: "$type",
-                                leadershipId: { $toObjectId: "$$r.titleId" }
-                            }
-                        } },
-                        { $map: {
-                            input: "$orMonitoringRemediation",
-                            as: "r",
-                            in: {
-                                role: "orMonitoringRemediation",
-                                title: "$$r.titleName",
-                                assignment: {
-                                    $reduce: {
-                                        input: {
-                                            $filter: {
-                                                input: "$$r.firmAssignments",
-                                                as: "fa",
-                                                cond: { $eq: ["$$fa.firmId", "$firmGroupId"] }
-                                            }
-                                        },
-                                        initialValue: "",
-                                        in: { $concat: ["$$value", "$$this.assignmentString"] }
-                                    }
+                                [{ role: "orIndependenceRequirement", title: "", assignment: "", fiscalYear: "$fiscalYear", memberFirmId: "$memberFirmId", country: "$country", firmGroupId: "$firmGroupId", name: "$name", type: "$type" }]
+                            ]
+                        },
+                        { 
+                            $cond: [
+                                { $gt: [{ $size: "$orMonitoringRemediation" }, 0] },
+                                {    
+                                    $map: {
+                                        input: "$orMonitoringRemediation",
+                                        as: "r",
+                                        in: {
+                                            role: "orMonitoringRemediation",
+                                            title: "$$r.titleName",
+                                            assignment: {
+                                                $reduce: {
+                                                    input: {
+                                                        $filter: {
+                                                            input: "$$r.firmAssignments",
+                                                            as: "fa",
+                                                            cond: { $eq: ["$$fa.firmId", "$firmGroupId"] }
+                                                        }
+                                                    },
+                                                    initialValue: "",
+                                                    in: { $concat: ["$$value", "$$this.assignmentString"] }
+                                                }
+                                            },
+                                            fiscalYear: "$fiscalYear",
+                                            memberFirmId: "$memberFirmId",
+                                            country: "$country",
+                                            firmGroupId: "$firmGroupId",
+                                            name: "$name",
+                                            type: "$type",
+                                            leadershipId: { $toObjectId: "$$r.titleId" }
+                                        }
+                                    } 
                                 },
-                                fiscalYear: "$fiscalYear",
-                                memberFirmId: "$memberFirmId",
-                                country: "$country",
-                                firmGroupId: "$firmGroupId",
-                                name: "$name",
-                                type: "$type",
-                                leadershipId: { $toObjectId: "$$r.titleId" }
-                            }
-                        } }
+                                [{ role: "orMonitoringRemediation", title: "", assignment: "", fiscalYear: "$fiscalYear", memberFirmId: "$memberFirmId", country: "$country", firmGroupId: "$firmGroupId", name: "$name", type: "$type" }]
+                            ]
+                        }
                     ]
                 }
             }
@@ -284,6 +286,8 @@ try{
         { $sort: { fiscalYear: 1, memberFirmId: 1 } },
         { $out: 'sqmleadership' }
     ], { allowDiskUse: true });
+    
+    // db.titleAndAssignments.drop();
 
     var endTime = new Date();
     print("SYSTEM:Power-BI-Info:: Completed SQM Leadership Roles script at ", endTime.toISOString(), " Total execution time (minutes): ", (endTime.getTime() - startTime.getTime())/60000);
