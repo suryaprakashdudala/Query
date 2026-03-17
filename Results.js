35f693334d990732b4a732ec8322ce4871938f0d
Satya  Suryaprakash Dudala <satya.suryaprakash.dudala@ey.com>
2026-03-17 21:29:57 +0530

    Added results

---
+++ Results.js
@@ -0,0 +1,110 @@
+
+  //sqmleadership findOne result
+  {
+    _id: ObjectId('69b979a8c7abaa9cabb911ea'),
+    ultimateResponsibilityResolved: {
+      memberFirmId: 10079,
+      fiscalYear: 2026,
+      country: 'India',
+      firmGroupId: 'IN',
+      name: 'Ernst & Young LLP',
+      type: 'EntityType_MemberFirm',
+      role: 'ultimateResponsibility',
+      leadershipId: '6855d87771835f94ed69e686'
+    }
+  },
+  {
+    _id: ObjectId('69b979a8c7abaa9cabb911eb'),
+    ultimateResponsibilityResolved: {
+      memberFirmId: 10079,
+      fiscalYear: 2026,
+      country: 'India',
+      firmGroupId: 'IN',
+      name: 'Ernst & Young LLP',
+      type: 'EntityType_MemberFirm',
+      role: 'ultimateResponsibility',
+      leadershipId: '6855d87671835f94ed69e647'
+    }
+  },
+  {
+    _id: ObjectId('69b979a8c7abaa9cabb911ec'),
+    ultimateResponsibilityResolved: {
+      memberFirmId: 10079,
+      fiscalYear: 2026,
+      country: 'India',
+      firmGroupId: 'IN',
+      name: 'Ernst & Young LLP',
+      type: 'EntityType_MemberFirm',
+      role: 'ultimateResponsibility',
+      leadershipId: '6855d87771835f94ed69e67b'
+    }
+  },
+  {
+    _id: ObjectId('69b979a8c7abaa9cabb911ed'),
+    ultimateResponsibilityResolved: {
+      memberFirmId: 10079,
+      fiscalYear: 2026,
+      country: 'India',
+      firmGroupId: 'IN',
+      name: 'Ernst & Young LLP',
+      type: 'EntityType_MemberFirm',
+      role: 'ultimateResponsibility',
+      leadershipId: '6855d87771835f94ed69e67d'
+    }
+  },
+
+//results from the firm collection
+ {
+    _id: ObjectId('6855d86871835f94ed69e395'),
+    abbreviation: null,
+    attachments: [],
+    country: 'India',
+    functionOwners: [],
+    firmGroupId: 'IN',
+    isPartOfGroup: 'IsPartOfGroupType_Yes',
+    name: 'Ernst & Young LLP',
+    parentFirmId: null,
+    type: 'EntityType_MemberFirm',
+    ultimateResponsibility: [ '6855d87771835f94ed69e686' ],
+    operationalResponsibilitySqm: [ '6855d87671835f94ed69e647' ],
+    orIndependenceRequirement: ObjectId('6855d87771835f94ed69e67b'),
+    orMonitoringRemediation: ObjectId('6855d87771835f94ed69e67d'),
+    publishedDate: '',
+    publishedBy: '',
+    publishedUserDisplayName: '',
+    isPublishQueryRun: '',
+    modifiedBy: 'SYSTEM_ROLLFORWARD',
+    modifiedOn: '2025-12-01T11:13:26.855Z',
+    createdOn: '2025-06-20T21:53:41.727Z',
+    createdBy: 'SYSTEM_ROLLFORWARD',
+    fiscalYear: 2026,
+    isRollForwardedFromPreFY: true,
+    isRapValidationEnabled: false,
+    prevId: '6647e366c3f707c7653c4168',
+    isRollForwardTriggered: false,
+    isRollFwdComplete: false,
+    rollForwardDate: '',
+    rollForwardStatus: '',
+    rollForwardByEmail: '',
+    rollForwardByDisplayName: '',
+    isReadOnly: false,
+    geographyId: '/null/',
+    archivalException: '',
+    archivalStatus: '',
+    isArchivalComplete: false,
+    memberFirmId: 10079,
+    archivedByDisplayName: '',
+    archivedByEmail: '',
+    archivedDate: '',
+    isCreatedInCurrentFY: false,
+    firstPublishedDate: '',
+    isFirmSaved: false,
+    isMFProfilingReviewed: false,
+    categoryType: '',
+    pcaobOperationalResponsibilitySqm: '',
+    pcaobOrIndependenceRequirement: '',
+    pcaobOrMonitoringRemediation: '',
+    pcaobOrMonitoringRemediationComponents: '',
+    pcaobUltimateResponsibility: '',
+    location: 'NL'
+  }
