16e9c571067b3021c628819baa1103cfb3f5384b
Satya  Suryaprakash Dudala <satya.suryaprakash.dudala@ey.com>
2026-04-01 17:47:23 +0530

    Added results

---
+++ results of debug_requirement_assignment.js
@@ -0,0 +1,12 @@
+[
+  {
+    EntityId: 'USA',
+    Ref_UniqueId: 'RC-NTW-452',
+    isQoOverrideEnabled: '',
+    is_USA: true,
+    objectives_stage_1_content: [],
+    objectives_stage_2_content: [],
+    associatedQualityObjectives: [],
+    QualityObjectiveUniquesIds: ''
+  }
+]
