f1a7230b9303356a3c9d163fa3421fc3569faa92
Satya  Suryaprakash Dudala <satya.suryaprakash.dudala@ey.com>
2026-04-01 18:00:41 +0530

    updated result

--- results of debug_requirement_assignment.js
+++ results of debug_requirement_assignment.js
@@ -2,11 +2,19 @@
   {
     EntityId: 'USA',
     Ref_UniqueId: 'RC-NTW-452',
-    isQoOverrideEnabled: '',
+    isQoOverrideEnabled_raw: '',
     is_USA: true,
     objectives_stage_1_content: [],
-    objectives_stage_2_content: [],
+    objectives_after_filter: [],
     associatedQualityObjectives: [],
+    reBacPolicyQOIds: [
+      'QO-AME-13',  'QO-NTW-137',
+      'QO-NTW-138', 'QO-NTW-139',
+      'QO-NTW-140', 'QO-NTW-143',
+      'QO-NTW-144', 'QO-NTW-145',
+      'QO-NTW-148', 'QO-NTW-149',
+      'QO-USLI-02'
+    ],
     QualityObjectiveUniquesIds: ''
   }
 ]
