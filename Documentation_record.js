c87ffaba5b84ff3c842a52da2cca23395c4fdf9b
Satya  Suryaprakash Dudala <satya.suryaprakash.dudala@ey.com>
2026-04-14 22:15:14 +0530

    data added

---
+++ Documentation_record.js
@@ -0,0 +1,184 @@
+[
+  {
+    _id: ObjectId('6904f11565291dda74fa38f7'),
+    id: '6904f11565291dda74fa38f7',
+    pcaobValues: {
+      relatedObjectives: [],
+      relatedQualityRisks: [],
+      relatedSubRisks: [],
+      relatedProcesses: [],
+      mitigatedResources: [],
+      supportingITApplication: [],
+      addInformationExecutionControls: []
+    },
+    isqmValues: {
+      relatedObjectives: [ 'QO-NTW-20' ],
+      relatedQualityRisks: [ 'QR-NTW-48' ],
+      relatedSubRisks: [],
+      relatedProcesses: [ 'PR-57' ],
+      mitigatedResources: [],
+      supportingITApplication: [],
+      addInformationExecutionControls: []
+    },
+    componentId: '',
+    qualityObjectiveId: '',
+    order: 1153,
+    qualityObjectiveUniqueId: '',
+    relatedObjectives: [ 'QO-NTW-20' ],
+    notApplicable: false,
+    addInformationExecutionControls: [],
+    iecRationale: '',
+    iecRationaleAlt: '',
+    addControlEvidenceControls: [
+      {
+        name: '1: Email approval of the Business Case for the new DAP role by the Regional/ Country Service Line Leader (or delegate), Regional/ Country Managing Partner (or delegate), Regional/ Country Talent Leader (or delegate) and Regional/ Country COO (or delegate)',
+        nameAlt: '',
+        id: '17758059104917262501',
+        uniqueId: '7262501'
+      }
+    ],
+    controlEvidence: '',
+    controlName: 'Review and approval of Business Case for DAP needs',
+    controlNameAlt: '',
+    relatedPolicy: 'TBD',
+    relatedPolicyAlt: '',
+    controlCriteria: 'The Regional/ Country Service Line Leader (or delegate), Regional/ Country  Managing Partner (or delegate), Regional/ Country Talent Leader (or delegate) and Regional/ Country  COO (or delegate) reviews and approves the Business Case for a new DAP need which includes the following considerations:\n' +
+      '- The business need for the open DAP role\n' +
+      '- Why the firm should invest in this role\n' +
+      '- Candidate qualifications\n' +
+      '- ROI',
+    controlCriteriaAlt: '',
+    controlNature: 'ControlNature_Prevent',
+    controlOperator: [ ObjectId('6855d87b71835f94ed69e7ca') ],
+    controlType: 'ControlType_Manual',
+    controlFunction: [ '6855d86f71835f94ed69e4b3' ],
+    localControlFunction: [ '6855d86f71835f94ed69e4b3' ],
+    frequencyType: [ 'FrequencyType_EventDriven' ],
+    keyControlObjectiveAlt: '',
+    relatedSubRisks: [],
+    responseOwners: [ ObjectId('68ac1b425d7147a04b747d93') ],
+    categoryofService: '',
+    categoryServiceProvider: '',
+    status: null,
+    firmsNotApplicable: [],
+    engagementType: '',
+    typeOfEngagements: [
+      'TypeOfEngagements_AgreedUponProcedures',
+      'TypeOfEngagements_AuditOfFinancialStatements',
+      'TypeOfEngagements_Compilations',
+      'TypeOfEngagements_OtherAssuranceEngagements',
+      'TypeOfEngagements_ReviewsOfFinancialStatements'
+    ],
+    isControlOverResource: false,
+    resourceType: '',
+    mitigatedResources: [],
+    tags: [],
+    lockedBy: '',
+    lockedByGivenName: '',
+    lockedOn: '',
+    lockedByGeo: '',
+    supportingITApplication: [],
+    dateSavedAfterPublish: '',
+    assignedEntityTypes: [ 'EntityType_Region' ],
+    isSaved: false,
+    isRejected: false,
+    requirementControlId: '6855dc0371835f94ed69ef7e',
+    requirementControlFirmId: 'NTW',
+    apmId: [],
+    link: '',
+    linkToUserGuide: '',
+    relatedFunction: '',
+    techRelatedFunction: '',
+    relatedFunctionList: [],
+    techRelatedFunctionList: [],
+    relatedQualityRisksMitigatedByResource: [],
+    nameOfServiceProvider: '',
+    isResourceProvidedByService: false,
+    resourceSubSections: [],
+    riskMitigatedByResource: true,
+    riskMitigatedBySubsection: false,
+    resourcetype: '',
+    relatedQualityRisks: [ 'QR-NTW-48' ],
+    serviceLine: [
+      'ServiceLine_Assurance',
+      'ServiceLine_Consulting',
+      'ServiceLine_GDS',
+      'ServiceLine_Tax'
+    ],
+    attachments: [],
+    description: 'On an as needed basis, the Regional/ Country Service Line Leader (or delegate), Regional/ Country  Managing Partner (or delegate), Regional/ Country Talent Leader (or delegate) and Regional/ Country  COO (or delegate) reviews and approves the Business Case for a new direct admit partner (DAP), via email, in line with business needs. The Business Case is created by the Sub-Service Line Hiring Manager (Sponsoring Partner)',
+    descriptionAlt: '',
+    firmId: 'AGDS',
+    geographyId: '/AGDS/',
+    name: '',
+    nameAlt: '',
+    uniqueId: 'RC-NTW-153',
+    isParent: false,
+    type: 'RequirementControlAssignment',
+    baselineOriginalHierarchy: {
+      relatedQualityRisks: [ 'QR-NTW-48' ],
+      relatedSubRisks: [],
+      mitigatedResources: []
+    },
+    baselineModifiedAttributes: {
+      addControlEvidenceControls: [
+        {
+          name: '1: Email approval of the Business Case for the new DAP role by the Regional/ Country Service Line Leader (or delegate), Regional/ Country Managing Partner (or delegate), Regional/ Country Talent Leader (or delegate) and Regional/ Country COO (or delegate)',
+          nameAlt: ''
+        }
+      ],
+      controlCriteria: 'The Regional/ Country Service Line Leader (or delegate), Regional/ Country  Managing Partner (or delegate), Regional/ Country Talent Leader (or delegate) and Regional/ Country  COO (or delegate) reviews and approves the Business Case for a new DAP need which includes the following considerations:\n' +
+        '- The business need for the open DAP role\n' +
+        '- Why the firm should invest in this role\n' +
+        '- Candidate qualifications\n' +
+        '- ROI',
+      controlOperator: [ ObjectId('6855d87b71835f94ed69e7ca') ],
+      description: 'On an as needed basis, the Regional/ Country Service Line Leader (or delegate), Regional/ Country  Managing Partner (or delegate), Regional/ Country Talent Leader (or delegate) and Regional/ Country  COO (or delegate) reviews and approves the Business Case for a new direct admit partner (DAP), via email, in line with business needs. The Business Case is created by the Sub-Service Line Hiring Manager (Sponsoring Partner)',
+      responseOwners: [ ObjectId('68ac1b425d7147a04b747d93') ],
+      assignedEntityTypes: [ 'EntityType_Region' ]
+    },
+    baselineAcceptedAttributes: {
+      addControlEvidenceControls: [
+        {
+          name: '1: Email approval of the Business Case for the new DAP role by the Regional/ Country Service Line Leader (or delegate), Regional/ Country Managing Partner (or delegate), Regional/ Country Talent Leader (or delegate) and Regional/ Country COO (or delegate)',
+          nameAlt: ''
+        }
+      ],
+      controlCriteria: 'The Regional/ Country Service Line Leader (or delegate), Regional/ Country  Managing Partner (or delegate), Regional/ Country Talent Leader (or delegate) and Regional/ Country  COO (or delegate) reviews and approves the Business Case for a new DAP need which includes the following considerations:\n' +
+        '- The business need for the open DAP role\n' +
+        '- Why the firm should invest in this role\n' +
+        '- Candidate qualifications\n' +
+        '- ROI',
+      controlOperator: [ ObjectId('6855d87b71835f94ed69e7ca') ],
+      description: 'On an as needed basis, the Regional/ Country Service Line Leader (or delegate), Regional/ Country  Managing Partner (or delegate), Regional/ Country Talent Leader (or delegate) and Regional/ Country  COO (or delegate) reviews and approves the Business Case for a new direct admit partner (DAP), via email, in line with business needs. The Business Case is created by the Sub-Service Line Hiring Manager (Sponsoring Partner)',
+      responseOwners: [ ObjectId('68ac1b425d7147a04b747d93') ]
+    },
+    updatedOn: '2026-04-10T07:25:17.772Z',
+    communication: '',
+    categoryofResource: '',
+    isBaselineSaved: true,
+    isFunctionExcutedDifferent: false,
+    executionControlFunction: [ '6855d86f71835f94ed69e4b3' ],
+    nameOfControlServiceProvider: '',
+    isControlOperatedByServiceProvider: false,
+    nameOfControlServiceProviderAlt: '',
+    fiscalYear: 2026,
+    keyControlObjective: 'To review and approve the Business Case for hiring a new partner based on business needs',
+    prevId: '6647e502c3f707c7653c4c4a',
+    ctrlStateUnchangedFromPrevYear: false,
+    rollForwardReviewDate: '',
+    isDeleted: '',
+    isReadOnly: false,
+    fromLockUnlock: false,
+    dismissalofMitigatedresource: false,
+    additionalQOs: [],
+    additionalQRs: [],
+    additionalSRs: [],
+    isPCAOBRegistered: false,
+    isQoOverrideEnabled: '',
+    modifiedOn: '2026-04-13T09:11:54.033Z',
+    createdOn: '2025-10-31T17:23:48.495Z',
+    createdBy: 'SYSTEM_ROLLFORWARD',
+    modifiedBy: 'INC14508453'
+  }
+]
